73d45c2611827841e197faf75ac0f63c95c7d7d7
manos <adamantioszaras@gmail.com>
2017-06-02 15:14:03 +0300

    Correct search movie

--- searchMovie.php
+++ searchMovie.php
@@ -4,11 +4,11 @@ include 'connParameters.php';
 $con = mysqli_connect($host, $userName, $password, $dbName);
 
 if (!$con) {
-  die('Could not connect: ' . mysqli_error($con));
+  die('Could not connect: ' . mysqli_connect_error());
 }
 
 mysqli_select_db($con, $dbName);
-$sql="select * from movies where author,title,genre,price like" . $_GET["pattern"];
+$sql="select * from movies where (`author`,`title`,`genre`,`price`) like" . $_GET["pattern"];
 $result = mysqli_query($con, $sql);
 
 header("Content-type: application/json");
