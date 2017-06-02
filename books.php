da19e6891981d6f4a0bbc89ff024b9deffd26f19
manos <adamantioszaras@gmail.com>
2017-06-02 20:02:24 +0300

    Try to return json.

--- books.php
+++ books.php
@@ -35,11 +35,15 @@ else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $sql="insert into books (author, title, genre, price) values ('" .
             $author . "', '" . $title . "','" . $genre . "','" . $price . "')";
 
-    if (!mysqli_query($con, $sql)) {
-        die('Error: ' . mysqli_error($con));
-    }
+    $response = array();
 
-    echo "1 record added";
+    if (!mysqli_query($con, $sql)) {
+        $response['message'] = 'Error!';
+    }
+    else
+        $response['message'] = 'Success!'
+
+    print json_encode(array_values($response));
 }
 
 mysqli_close($con);
