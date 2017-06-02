1847b4f51e0d1f48e825d77885708c1f3fa8ccac
manos <adamantioszaras@gmail.com>
2017-06-02 16:47:54 +0300

    Change addBook.

--- addBook.php
+++ addBook.php
@@ -4,13 +4,13 @@ include 'connParameters.php';
 $con = mysqli_connect($host, $userName, $password, $dbName);
 
 if (!$con) {
-    die('Could not connect: ' . mysqli_error($con));
+  die('Could not connect: ' . mysqli_connect_error());
 }
 
-$author = mysqli_real_escape_string($con, $_GET['author']);
-$title = mysqli_real_escape_string($con, $_GET['title']);
-$genre = mysqli_real_escape_string($con, $_GET['genre']);
-$price = mysqli_real_escape_string($con, $_GET['price']);
+$author = mysqli_real_escape_string($con, $_POST['author']);
+$title = mysqli_real_escape_string($con, $_POST['title']);
+$genre = mysqli_real_escape_string($con, $_POST['genre']);
+$price = mysqli_real_escape_string($con, $_POST['price']);
 
 $sql="insert into books (author, title, genre, price) values ('" .
         $author . "', '" . $title . "','" . $genre . "','" . $price . "')";
@@ -18,6 +18,7 @@ $sql="insert into books (author, title, genre, price) values ('" .
 if (!mysqli_query($con, $sql)) {
   die('Error: ' . mysqli_error($con));
 }
+
 echo "1 record added";
 
 mysqli_close($con);
