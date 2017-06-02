49d7ab4a57d8f8bbf17ed9bcf198292537015810
manos <adamantioszaras@gmail.com>
2017-06-02 16:08:01 +0300

    Test.

--- searchBook.php
+++ searchBook.php
@@ -9,10 +9,10 @@ if (!$con) {
 
 mysqli_select_db($con, $dbName);
 
-$sql="select * from books where author like %" . $_GET["pattern"] .
-                                           "% or title like %" . $_GET["pattern"] .
-                                           "% or genre like %" . $_GET["pattern"] .
-                                           "% or price like %" . $_GET["pattern"] . "%"
+$sql="select * from books where author like %" . $_GET['pattern'] .
+                                           "% or title like %" . $_GET['pattern'] .
+                                           "% or genre like %" . $_GET['pattern'] .
+                                           "% or price like %" . $_GET['pattern'] . "%"
 
 $result = mysqli_query($con, $sql);
 
